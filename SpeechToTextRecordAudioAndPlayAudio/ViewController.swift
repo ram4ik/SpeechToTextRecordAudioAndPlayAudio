35efebfcc9477a2a7cdf005326c33788bee9337e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-10 13:54:08 +0300

    Initial Commit

---
+++ SpeechToTextRecordAudioAndPlayAudio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechToTextRecordAudioAndPlayAudio
+//
+//  Created by ramil on 10.10.2019.
+//  Copyright © 2019 com.ri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
